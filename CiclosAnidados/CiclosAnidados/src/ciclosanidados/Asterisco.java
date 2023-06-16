c3f4e671328cbece664ea3d16e0f4cd374cb104a
xaxbe <126003006+xaxbe@users.noreply.github.com>
2023-06-16 13:28:25 -0600

    Add files via upload

---
+++ CiclosAnidados/CiclosAnidados/src/ciclosanidados/Asterisco.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ciclosanidados;
+
+/**
+ *
+ * @author xaxbe
+ */
+
+//segundo ejemplo
+
+public class Asterisco {
+    public void Ast(){
+            for(int i = 1; i <= 5; i++) {
+    for(int j = 1; j <= i; j++) {
+        System.out.print("* ");
+    }
+    System.out.println();
+}
+}    
+}
+    
---
+++ CiclosAnidados/CiclosAnidados/src/ciclosanidados/CiclosAnidados.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ciclosanidados;
+
+/**
+ *
+ * @author xaxbe
+ */
+public class CiclosAnidados {
+//primer ejemplo
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++) {
+    for(int j = 1; j <= 10; j++) {
+        System.out.print(i*j + "\t");
+    }
+    System.out.println();
+}
+
+    }
+    
+}
